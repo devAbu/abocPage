c9967955e59ddd0ac49d8cea08247e902a77f5f2
muhamedhas <hasanspahic.muhamed@gmail.com>
2018-12-03 15:45:25 +0100

    test

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'mysql');//mysql valjda za mac :P 
+define('DB_PASSWORD', '');//mysql valjda za mac :P
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'abocAbout');
 
@@ -9,3 +9,4 @@ define('DB_NAME', 'abocAbout');
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('could not connect' . mysqli_connect_error());
 
 ?>
+test
