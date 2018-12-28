2e250750fc4e7435ae2d3bca9319243b4a60b9e1
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2018-12-28 18:46:47 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 define('DB_USER', 'newuser');
 define('DB_PASSWORD', 'password');//mysql valjda za mac :P
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'abocAbout');
+define('DB_NAME', 'abocabout');
 
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('could not connect' . mysqli_connect_error());
